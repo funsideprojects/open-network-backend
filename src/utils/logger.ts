159115da0801773461905681d2cca319941eccff
lPaths <tienphat1501@gmail.com>
2020-05-27 13:48:04 +0700

    update

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,4 @@
-import * as chalk from 'chalk'
+import * as hl from 'utils/chalk'
 
 const LOG_TYPES = {
   NONE: 0,
@@ -12,24 +12,24 @@ const logType = (process.env.LOG_TYPE && +process.env.LOG_TYPE) || LOG_TYPES.NOR
 const logTime = () => {
   const now = new Date()
 
-  return chalk.yellowBright(
+  return hl.warning(
     `[${now.toLocaleDateString()} ${now.toLocaleTimeString([], { hour12: false })}]`
   )
 }
 
 const log = (...args) => {
   if (logType < LOG_TYPES.NORMAL) return
-  console.log(logTime(), `[${process.pid}]`, chalk.greenBright.bold('[INFO]'), ...args)
+  console.log(logTime(), `[${process.pid}]`, hl.info('[INFO]'), ...args)
 }
 
 const error = (...args) => {
   if (logType < LOG_TYPES.ERROR) return
-  console.log(logTime(), `[${process.pid}]`, chalk.redBright.bold('[ERROR]'), ...args)
+  console.log(logTime(), `[${process.pid}]`, hl.error('[ERROR]'), ...args)
 }
 
 const debug = (...args) => {
   if (logType < LOG_TYPES.DEBUG) return
-  console.log(logTime(), `[${process.pid}]`, chalk.blueBright.bold('[DEBUG]'), ...args)
+  console.log(logTime(), `[${process.pid}]`, hl.warning('[DEBUG]'), ...args)
 }
 
 export default { log, error, debug }
